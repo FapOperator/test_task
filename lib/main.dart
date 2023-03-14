84ce02db695c9679664ea25bbf0d96c267614e58
FapOperator <KDA@d.c>
2023-03-14 19:49:46 +0200

    test task

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:test_task/common/list_string.dart';
+import 'package:test_task/read_more_text.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    String list = listString.join(", ");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          OverflowDetectorText(
+            child: Text(
+              list,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OverflowDetectorText extends StatelessWidget {
+  final Text child;
+
+  const OverflowDetectorText({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //временная вспомогательная переменная для текста
+    List<String> supportTextParams = [];
+    // отформатированный в конечный результат текст
+    String mainText = '';
+    // число строк при переполнении
+    int? linesCountCollapse;
+    //дефолтное значение числа строк текста без переполнения
+    int linesCountFullText = 2;
+
+    //вычисление размера body экрана
+    double bodyHeight = (MediaQuery.of(context).size.height) -
+        (Scaffold.of(context).appBarMaxHeight!) -
+        MediaQuery.of(context).padding.top;
+
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        // цикл нужен для того чтоб словить слово после которого происходит переполнение
+        for (int i = 0; i < listString.length; i++) {
+          supportTextParams.add(listString[i]);
+          mainText = supportTextParams.join(", ");
+          // позволяет узнать высоту текста при каждой итерации цикла
+          TextPainter tp2 = TextPainter(
+            maxLines: child.maxLines,
+            textAlign: child.textAlign ?? TextAlign.start,
+            textDirection: child.textDirection ?? TextDirection.ltr,
+            text: child.textSpan ??
+                TextSpan(
+                  text: mainText,
+                  style: child.style,
+                ),
+          );
+          // указание ширины текста
+          tp2.layout(maxWidth: constrains.maxWidth);
+          //вычисление максимальное возможного количества строк на данной итерации, преобразование высот, в понятное число для maxLines
+          linesCountFullText = (bodyHeight / tp2.preferredLineHeight).ceil();
+          //если высота текста превышает высоту body, то true
+          if (tp2.size.height > bodyHeight) {
+            linesCountCollapse =
+                (tp2.size.height / tp2.preferredLineHeight).ceil() - 1;
+            break;
+          }
+        }
+        //отрисовка конечного текста на экране
+        return Column(
+          children: [
+            ReadMoreText(
+              mainText,
+              trimLines: linesCountCollapse ?? linesCountFullText,
+              trimCollapsedText:
+                  " + ${(listString.length - supportTextParams.length).toString()} больше",
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/read_more_text.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class ReadMoreText extends StatefulWidget {
+  const ReadMoreText(
+    this.data, {
+    Key? key,
+    this.trimCollapsedText = ' ...больше',
+    this.colorClickableText,
+    required this.trimLines,
+  }) : super(key: key);
+
+  final String data;
+  final String trimCollapsedText;
+  final Color? colorClickableText;
+  final int trimLines;
+
+  @override
+  ReadMoreTextState createState() => ReadMoreTextState();
+}
+
+const String _kEllipsis = '\u2026';
+
+class ReadMoreTextState extends State<ReadMoreText> {
+  @override
+  Widget build(BuildContext context) {
+    //передача текста и назначение стилей плашке "больше"
+    TextSpan link = TextSpan(
+      text: widget.trimCollapsedText,
+      style: const TextStyle(color: Colors.red),
+    );
+
+    Widget result = LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        assert(constraints.hasBoundedWidth);
+        final double maxWidth = constraints.maxWidth;
+
+        // Создание текста
+        final text = TextSpan(
+          text: widget.data,
+        );
+
+        // размеры кнопки больше
+        TextPainter textPainter = TextPainter(
+          text: link,
+          textDirection: TextDirection.ltr,
+          maxLines: widget.trimLines,
+          ellipsis: _kEllipsis,
+        );
+        // указание ширины текста
+        textPainter.layout(minWidth: constraints.minWidth, maxWidth: maxWidth);
+        //вычисление размера плашки больше
+        final linkSize = textPainter.size;
+
+        // размеры текста
+        textPainter.text = text;
+        // указание ширины текста
+        textPainter.layout(minWidth: constraints.minWidth, maxWidth: maxWidth);
+        //вычисление размера плашки больше
+        final textSize = textPainter.size;
+
+        // Получение последнего индекса строки
+        int endIndex;
+
+        if (linkSize.width < maxWidth) {
+          final pos = textPainter.getPositionForOffset(Offset(
+            textSize.width - linkSize.width,
+            textSize.height,
+          ));
+          endIndex = textPainter.getOffsetBefore(pos.offset)!;
+        } else {
+          var pos = textPainter.getPositionForOffset(
+            textSize.bottomLeft(Offset.zero),
+          );
+          endIndex = pos.offset;
+        }
+
+        TextSpan textSpan;
+        // если есть переполнение добавляется плашка "больше"
+        if (textPainter.didExceedMaxLines) {
+          textSpan = TextSpan(
+            text: widget.data.substring(0, endIndex),
+            children: <TextSpan>[link],
+          );
+        } else {
+          textSpan = TextSpan(
+            text: widget.data,
+          );
+        }
+        //отрисовка виджета
+        return RichText(
+          textDirection: TextDirection.ltr,
+          softWrap: true,
+          overflow: TextOverflow.clip,
+          text: textSpan,
+        );
+      },
+    );
+
+    return result;
+  }
+}
